0a9cbe12b95fb57719361d6e9b2ca052560c6500
Fahad Siddiqui <56387313+fahad-git@users.noreply.github.com>
2025-05-21 21:52:50 +0200

    GMP-1414: Refactor: updated store provider by removing unused statements for clean code best practices.

--- frontend/src/app/StoreProvider.tsx
+++ frontend/src/app/StoreProvider.tsx
@@ -1,43 +1,46 @@
 /**
- * @description: This file contains store provided integration to the app. There will be only single instance of reduc store in the app.
- *               The store will be persistent store. Wrapping this store in strict mode for avoiding type confusions.
+ * @description: This file contains store store-provided integration to the app. There will be only a single instance of the reduce store in the app.
+ *               The store will be a persistent store. Wrapping this store in strict mode to avoid type confusion.
  */
 "use client";
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../handlers/redux/store";
 import { initialStoreDefaultAction } from "../handlers/redux/actions";
-// this PersistGate is used in case store has issues with persistent storage.
-// eslint-disable-next-line
+
 import { PersistGate } from "redux-persist/integration/react";
 import React from "react";
 import { persistStore } from "redux-persist";
 
-export default function StoreProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
+
   const storeRef = useRef<AppStore>();
+  
   let persistor: any;
+  
   if (!storeRef.current) {
+    
     // Create the store instance the first time this renders.
     storeRef.current = makeStore();
+    
     // make this store persistent.
-    // eslint-disable-next-line
     persistor = persistStore(storeRef.current);
+
     // initializing the first/initial state.
     storeRef.current.dispatch(initialStoreDefaultAction());
+    
   }
 
   return (
     <React.StrictMode>
+      
       <Provider store={storeRef.current}>
-        {/* Use persistGate if store creates issue with persistent storage. Uncommenting below line should fix possible persistent issue. */}
-        {/* <PersistGate loading={null} persistor={persistor}> */}
+        
+        {/* Use persistGate if store creates issue with persistent storage. Uncommenting the below line should fix a possible persistent issue. */}
         {children}
-        {/* </PersistGate> */}
+      
       </Provider>
+    
     </React.StrictMode>
   );
 }
